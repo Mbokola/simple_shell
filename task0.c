3b10d3bff44bac7d53ecf89067424d7089a82be7
Mbokola <tonypeaks@gmail.com>
2023-04-27 13:54:22 +0000

    Remix

--- task0.c
+++ task0.c
@@ -19,16 +19,26 @@ int main(void)
 			write(STDOUT_FILENO, "$ ", 2);
 		length = getline(&input, &n, stdin);
 		if (length == -1)
+		{
+			free(input);
 			break;
+		}
 		input[length - 1] = '\0';
 		tmp = malloc(sizeof(char *) * strlen(input) + 1);
 		tmp[1] = NULL;
 		tmp[0] = strdup(input);
 		child = fork();
 		if (child == 0)
-			execve(tmp[0], tmp, environ);
+		{
+			if (execve(tmp[0], tmp, environ) == -1)
+				perror("File not found");
+		}
 		else
 			wait(&status);
+		free(tmp[0]);
+		free(tmp);
+		free(input);
 	}
+	free(input);
 	return (0);
 }
